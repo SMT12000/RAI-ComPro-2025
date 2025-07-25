95420728f9bc39ec52e17438bd3120dbcea3dd2a
ItsSMT <68012000@kmitl.ac.th>
2025-07-25 17:21:42 +0000

    Write your commit note here

---
+++ lab-1/ex_02.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+
+{
+
+    printf("My Name is  : %s\n","Soe Myint Tun.");
+    printf("I am %d year old\n",17);
+    printf("I am %.2f cm tall\n",180.25);
+    
+    return 0;
+}
---
+++ lab-1/ex_04.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+
+{
+    float a,b,d;
+    int c;
+    a = 68.01;
+    b = 8.4;
+    c = 3;
+    d = a+b+c;
+    printf("%d\n",d);
+    return 0;
+
+}
