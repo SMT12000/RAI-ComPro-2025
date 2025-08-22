b8c8def2f27422ccbf86285fe70fce700f1905ca
ItsSMT <68012000@kmitl.ac.th>
2025-08-22 21:44:56 +0000

    main

--- lab-5/challenge.c
+++ lab-5/challenge.c
@@ -1,27 +1,35 @@
- #include <stdio.h>
+#include <stdio.h>
 
 int main() {
-    int n, i, j, count;
-    int numbers[n];
-    
+    int n, i, j, search, count;
+
     printf("Enter number of elements: ");
     scanf("%d", &n);
-    
+
+    int arr[n];
     printf("Enter %d integers: ", n);
-    for(i = 0; i < n; i++) {
-        scanf("%d", &numbers[i]);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
     }
 
-    for(i = 0; i < n; i++) {
-        count = 1;
-        if(numbers[i] != -1) {
-            for(j = i + 1; j < n; j++) {
-                if(numbers[i] == numbers[j]) {
+    printf("Output:\n");
+    for (i = 0; i < n; i++) {
+        int already_counted = 0;
+        for (j = 0; j < i; j++) {
+            if (arr[i] == arr[j]) {
+                already_counted = 1;
+                break;
+            }
+        }
+
+        if (!already_counted) {
+            count = 1;
+            for (j = i + 1; j < n; j++) {
+                if (arr[i] == arr[j]) {
                     count++;
-                    numbers[j] = -1;
                 }
             }
-            printf("Element %d occurs %d times\n", numbers[i], count);
+            printf("Element %d occurs %d times\n", arr[i], count);
         }
     }
 
